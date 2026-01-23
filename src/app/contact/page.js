451e53c12f819ed6486e04b0b6439c18eb0f25fa
Prajwal <prajwalbhandagi@gmail.com>
2026-01-23 17:53:48 +0530

    Fix contact page export

---
+++ src/app/contact/page.js
@@ -0,0 +1,9 @@
+'use client'
+
+export default function ContactPage() {
+    return (
+        <div style={{ padding: '100px', color: 'white' }}>
+            <h1>Contact Page</h1>
+        </div>
+    )
+}
