241b7319b824eb68c05a2f62d0bae5327f03ca34
Prajwal <prajwalbhandagi@gmail.com>
2026-02-08 22:05:18 +0530

    corrected layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -59,8 +59,9 @@ export default function RootLayout({ children }) {
             <head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-                <link rel="preconnect" href="https://5feet4r.vercel.app" />
+                <link rel="preconnect" href="https://5feet4.co" />
                 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+                <meta name="google-site-verification" content="KpOQf0rjQOqal4ybI7PdqUXSOJ0HcD7AKyzCRJze5IA" />
             </head>
             <body className={josefinSans.className} suppressHydrationWarning>
                 <ContextMenu />
