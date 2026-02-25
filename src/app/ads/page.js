be24b0cbfdb984cc149f2bd281c7c70ff9a5d9d6
Prajwal <prajwalbhandagi@gmail.com>
2026-02-25 15:14:27 +0530

    Updated

--- src/app/ads/page.js
+++ src/app/ads/page.js
@@ -794,13 +794,13 @@ export default function Ads() {
                         onClick={(e) => toggleSound(e, i)}
                       >
                         {isUnmuted ? (
-                          <svg viewBox="0 0 24 24" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                          <svg className="icon-muted" viewBox="0 0 24 24" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                             <polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5" />
                             <path d="M15.54 8.46a5 5 0 0 1 0 7.07" />
                             <path d="M19.07 4.93a10 10 0 0 1 0 14.14" />
                           </svg>
                         ) : (
-                          <svg viewBox="0 0 24 24" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                          <svg className="icon-unmuted" viewBox="0 0 24 24" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                             <polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5" />
                             <line x1="23" y1="9" x2="17" y2="15" />
                             <line x1="17" y1="9" x2="23" y2="15" />
